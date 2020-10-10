f8ee4198edcdfb61e181a92f4b0c274d3d5de489
seiya0429 <seiya.060429@gmail.com>
2020-10-11 00:35:05 +0900

    予約枠unitテスト通過

---
+++ app/Models/VacancyLevel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+class VacancyLevel
+{
+    private $remainingCount;
+
+    public function __construct(int $remainigCount)
+    {
+        $this->remainingCount = $remainigCount;
+    }
+
+    public function mark(): string
+    {
+        if ($this->remainingCount === 0) {
+            return '☓';
+        }
+
+        if ($this->remainingCount < 5) {
+            return '△';
+        }
+
+        return '◎';
+    }
+}
