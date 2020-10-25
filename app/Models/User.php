32b07e174832246807fdb97a0fed2929e54b7a63
seiya0429 <seiya.060429@gmail.com>
2020-10-25 15:00:51 +0900

    ＃2-4まで完了

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -37,14 +39,28 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function canReserve(int $remainingCount, int $reservationCount): bool
+    public function reservations(): HasMany
     {
-        if ($remainingCount === 0) {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function reservationCountThisMonth(): int
+    {
+        $today = Carbon::today();
+        return $this->reservations()
+            ->whereYear('created_at', $today->year)
+            ->whereMonth('created_at', $today->month)
+            ->count();
+    }
+
+    public function canReserve(Lesson $lesson): bool
+    {
+        if ($lesson->remainingCount() === 0) {
             return false;
         }
         if ($this->plan === 'gold') {
             return true;
         }
-        return $reservationCount < 5;
+        return $this->reservationCountThisMonth() < 5;
     }
 }
